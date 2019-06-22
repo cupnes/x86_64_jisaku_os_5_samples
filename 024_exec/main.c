84e431aba345a73ab24992298715d424c80ef5a0
Yuma Ohgami <yuma@ohgami.jp>
2019-06-22 20:00:05 +0900

    024_exec: update.

--- 024_exec/main.c
+++ 024_exec/main.c
@@ -21,21 +21,34 @@ struct __attribute__((packed)) platform_info {
 };
 
 #define INIT_APP	"test"
+#define MAX_APS		16
 
 /* コンソールの初期化が完了したか否か */
 unsigned char is_con_inited = 0;
 
 unsigned char get_pnum(void);
 
+struct file *ap_task[MAX_APS] = { NULL };
+
 void start_kernel(void *_t __attribute__((unused)), struct platform_info *pi,
 		  void *_fs_start)
 {
 	unsigned char pnum = get_pnum();
 
-	/* APの場合、初期化処理をスキップ */
+	/* APの場合、割り込み設定のみを行う */
 	if (pnum) {
-		/* コンソールの初期化が完了するまで待つ */
-		while (!is_con_inited);
+		/* CPU周りの初期化 */
+		gdt_init();
+		intr_init();
+
+		/* システムコールの初期化 */
+		syscall_init();
+
+		/* 自分用のタスクが登録されるのを待つ */
+		while (!ap_task[pnum - 1]);
+
+		/* 実行 */
+		exec(ap_task[pnum - 1]);
 
 		/* haltして待つ */
 		while (1)
@@ -48,13 +61,6 @@ void start_kernel(void *_t __attribute__((unused)), struct platform_info *pi,
 	set_bg(0, 70, 250);
 	clear_screen();
 	is_con_inited = 1;
-
-	/* プロセッサ数を表示 */
-	puth(pi->nproc, 2);
-
-	/* haltして待つ */
-	while (1)
-		cpu_halt();
 
 	/* ACPIの初期化 */
 	acpi_init(pi->rsdp);
@@ -74,6 +80,13 @@ void start_kernel(void *_t __attribute__((unused)), struct platform_info *pi,
 
 	/* ファイルシステムの初期化 */
 	fs_init(_fs_start);
+
+	/* AP1で外部アプリ実行 */
+	ap_task[0] = open("puta");
+
+	/* haltして待つ */
+	while (1)
+		cpu_halt();
 
 	/* スケジューラの初期化 */
 	sched_init();
